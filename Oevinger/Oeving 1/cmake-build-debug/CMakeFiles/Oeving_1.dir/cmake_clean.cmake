3a4380d55671877f8cb699cbb8325736df40b133
Ingebrigt <ing.hovind@hotmail.com>
2020-09-07 18:44:05 +0200

    initial commit

---
+++ Oevinger/Oeving 1/cmake-build-debug/CMakeFiles/Oeving_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oeving_1.dir/mem.c.o"
+  "Oeving_1"
+  "Oeving_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Oeving_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
